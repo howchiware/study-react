a8e47fadd18a534412eda233b1ba91344f3e44e5
haeun <howni0630@gmail.com>
2025-01-08 16:28:01 +0900

    내용 업데이트

--- 01-starting-project-new/src/index.jsx
+++ 01-starting-project-new/src/index.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
@@ -6,3 +6,5 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+//해당 APP 컴포넌트 함수의 포인터를 이 createElement에 전달하면 됨
+//ReactDOM.createRoot(entryPoint).render(React.createElement(App));
