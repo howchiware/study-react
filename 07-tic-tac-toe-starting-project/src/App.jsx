16f93e8f252d89ae4f4fea1730353672aea73e01
haeun <howni0630@gmail.com>
2025-01-11 16:06:04 +0900

    코드 업데이트

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react"; 
+
+import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
 
 function App() {
   
+  const [activePlayer, setActivePlayer] = useState('X');
+
+  function handleSelectSquare() {
+    setActivePlayer( (curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+  }
 
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        {/** 순서 있는 목록이 필요한 이유: 플레이어1, 2.. 이기 때문문 */}
+        <ol id='players' className="highlight-player">
+          <Player initailName="Player-1" symbol="X" isActive={activePlayer === 'X'}/>
+          <Player initailName="Player-2" symbol="O" isActive={activePlayer === 'O'}/>
+        </ol>
+
+        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+      </div>
+
+      LOG
+    </main>
+  );
 }
 
 export default App
---
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+export default function Player ({initailName, symbol, isActive}) {
+    const [playerName, setPalyerName] = useState(initailName);
+
+    const [isEditing, setIsEditing] = useState(false);
+
+    function handelEditClick() {
+        // isEditing 값이 true라면 setIsEditing에 false를 보냄
+        // setIsEditing(isEditing ? false : true);
+        // setIsEditing(!isEditing);
+        setIsEditing( (editing) => !editing);
+    }
+
+    function handleChange(event) {
+        setPalyerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+    //let btnCaption = 'Edit';
+
+    if(isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
+        //btnCaption = 'Save';
+    }
+
+    return (
+        <li className={isActive ? 'active' : undefined}>
+            <span className="player">
+              {editablePlayerName}
+              <span className='player-symbol'>{symbol}</span>
+            </span>
+            <button onClick={handelEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
+          </li>
+    );
+}
